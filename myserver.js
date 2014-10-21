1754fc988683d8e487f94060d34e761ddabcc39c
john gorter <john.gorter@gmail.com>
2014-10-21 12:11:10 +0200

    'ajax'

--- myserver.js
+++ myserver.js
@@ -1,8 +1,30 @@
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser')
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var app = express();
+var todo = [];
+
+todo.push({ id:0, title:"hond uitlaten", description:"wel laten poepen"});
+
+app.use(express.static(__dirname + "/app"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/todos", function(req, res){
+  res.setHeader("content-type", "application/json");
+  res.end(JSON.stringify(todo));
+});
+
+app.post("/todos", function(req, res){
+  var newtodo = {};
+  newtodo.title = req.body.title;
+  newtodo.description = req.body.description;
+  todo.push(newtodo);
+  
+  res.end("todo added");
+  });
+
+app.listen(1337);
 
 console.log('Server running at http://127.0.0.1:1337/');
