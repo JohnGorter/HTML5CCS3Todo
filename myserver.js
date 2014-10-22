9d40ca0d533c0b077c15931f87472fd95887335c
john gorter <john.gorter@gmail.com>
2014-10-22 14:16:54 +0200

    'mongodb'

--- myserver.js
+++ myserver.js
@@ -3,32 +3,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var less = require('less');
 var lessMiddleware = require('less-middleware');
+var mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost/todos');
+
+var todoSchema = mongoose.Schema({
+    title:String,
+    description:String
+})
+var todoClass = mongoose.model('Todo', todoSchema);
 
 var app = express();
-var todo = [];
-
-todo.push({ id:0, title:"hond uitlaten", description:"wel laten poepen"});
-
 app.use(lessMiddleware(__dirname + '/app'));
 app.use(express.static(__dirname + "/app"));
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+app.delete("/todos", function(req, res){
+  todoClass.findOneAndRemove({ title:req.query.title}, function (){
+    res.end("todo deleted");
+    });
+});
 
 app.get("/todos", function(req, res){
   res.setHeader("content-type", "application/json");
-  res.end(JSON.stringify(todo));
+  todoClass.find({}, function(err, data){
+      res.end(JSON.stringify(data));
+    });
 });
 
 app.post("/todos", function(req, res){
-  var newtodo = {};
-  newtodo.title = req.body.title;
-  newtodo.description = req.body.description;
-  todo.push(newtodo);
-  
+  var newtodo = new todoClass({ title:req.body.title, description:req.body.description});
+  newtodo.save(function(){
   res.end("todo added");
   });
+});
 
 app.listen(1337);
 
